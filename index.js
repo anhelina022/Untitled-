db1cdee4f6b23a5d1c068af7f8cc5a4411ba918a
Ангеліна Бебешко <angelinabebesko05@gmail.com>
2025-06-28 10:54:51 +0100

    add commit

--- index.js
+++ index.js
@@ -242,3 +242,114 @@ stopBtn.onclick = stopGame;
 window.onload = () => {
   cactus.style.animationPlayState = "paused";
 };
+
+
+//Обери вченого/их
+
+
+
+const scientists = [ 
+    { 
+        name: "Albert", 
+        surname: "Einstein", 
+        born: 1879, 
+        dead: 1955, 
+        id: 1 
+    }, 
+    { 
+        name: "Isaac", 
+        surname: "Newton", 
+        born: 1643, 
+        dead: 1727, 
+        id: 2 
+    }, 
+    { 
+        name: "Galileo", 
+        surname: "Galilei", 
+        born: 1564, 
+        dead: 1642, 
+        id: 3 
+    }, 
+    { 
+        name: "Marie", 
+        surname: "Curie", 
+        born: 1867, 
+        dead: 1934, 
+        id: 4 
+    }, 
+    { 
+        name: "Johannes", 
+        surname: "Kepler", 
+        born: 1571, 
+        dead: 1630, 
+        id: 5 
+    }, 
+    { 
+        name: "Nicolaus", 
+        surname: "Copernicus", 
+        born: 1473, 
+        dead: 1543, 
+        id: 6 
+    }, 
+    { 
+        name: "Max", 
+        surname: "Planck", 
+        born: 1858, 
+        dead: 1947, 
+        id: 7 
+    }, 
+    { 
+        name: "Katherine", 
+        surname: "Blodgett", 
+        born: 1898, 
+        dead: 1979, 
+        id: 8 
+    }, 
+    { 
+        name: "Ada", 
+        surname: "Lovelace", 
+        born: 1815, 
+        dead: 1852, 
+        id: 9 
+    }, 
+    { 
+        name: "Sarah E.", 
+        surname: "Goode", 
+        born: 1855, 
+        dead: 1905, 
+        id: 10 
+    }, 
+    { 
+        name: "Lise", 
+        surname: "Meitner", 
+        born: 1878, 
+        dead: 1968, 
+        id: 11 
+    }, 
+    { 
+        name: "Hanna", 
+        surname: "Hammarström", 
+        born: 1829, 
+        dead: 1909, 
+        id: 12 
+    } 
+];
+
+// let age = scientists.reduce(sum, scientists) => {
+//   return ;
+// };
+
+const items = document.querySelectorAll('.item-list');
+const btn = document.getElementById("list1");
+
+btn.addEventListener("click", (event) =>{
+  let scientistTh19 = scientists.filter(
+  (scientist) => scientist.born >= 1801
+  );
+  items.forEach((element, idx) => {
+    if (scientistTh19[idx]) {
+      element.textContent = `${scientistTh19[idx].name} ${scientistTh19[idx].surname}`;
+    }
+  });
+});
+
