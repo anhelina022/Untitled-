412bbaa7b944d90105ff6b872cd95ad31ecf20c6
Ангеліна Бебешко <angelinabebesko05@gmail.com>
2025-07-05 22:27:16 +0100

    add commit

--- index.js
+++ index.js
@@ -14,7 +14,11 @@ function checkLeapYear() {
   }
 }
 
+
+
 // Вгадай число, яке загадав комп’ютер
+
+
 
 function generateRandomNumber() {
   return Math.floor(Math.random() * 10) + 1;
@@ -39,7 +43,11 @@ function checkGuess() {
   randomNumber = generateRandomNumber();
 }
 
+
+
 // Камінь - ножиці - папір
+
+
 
 let userScore = 0;
 let computerScore = 0;
@@ -78,7 +86,11 @@ function computerPlay() {
   alert(`Комп'ютер вибрав: ${computerChoice}`);
 }
 
+
+
 // Калькулятор
+
+
 
 function calculate(operation) {
   let a = parseFloat(document.getElementById("input-number").value);
@@ -120,7 +132,10 @@ function calculate(operation) {
   document.getElementById("result").innerHTML = result;
 }
 
+
 //Калькулятор часу
+
+
 function calculateTime() {
   let seconds = parseInt(document.getElementById("inputNumber").value);
   if (isNaN(seconds) || seconds < 0) {
@@ -138,60 +153,11 @@ function calculateTime() {
   ).textContent = `${days} дн. ${hours}:${minutes}:${seconds}`;
 }
 
-// Введіть 3 числа
 
-const input1 = document.getElementById("input1");
-const input2 = document.getElementById("input2");
-const input3 = document.getElementById("input3");
-const output = document.getElementById("myParagraph");
-
-// Додаємо слухачі подій input
-input1.addEventListener("input", handleInput);
-input2.addEventListener("input", handleInput);
-input3.addEventListener("input", handleInput);
-
-function handleInput() {
-  const val1 = input1.value.trim();
-  const val2 = input2.value.trim();
-  const val3 = input3.value.trim();
-
-  const num1 = parseFloat(val1);
-  const num2 = parseFloat(val2);
-  const num3 = parseFloat(val3);
-
-  if (val1 === "" || val2 === "" || val3 === "") {
-    output.textContent = "Введіть усі три числа.";
-    return;
-  }
-
-  if (isNaN(num1) || isNaN(num2) || isNaN(num3)) {
-    output.textContent = "Будь ласка, введіть тільки числові значення.";
-    return;
-  }
-
-  const max = Math.max(num1, num2, num3);
-  output.textContent = `Найбільше число, яке ви ввели - ${max}`;
-}
-
-// Футбол
-
-const field = document.getElementById("field");
-const ball = document.getElementById("ball");
-const ballRadius = ball.offsetWidth / 2;
-field.addEventListener("click", ({ clientX, clientY }) => {
-  const { left, top, width, height } = field.getBoundingClientRect();
-  const x = Math.min(
-    Math.max(clientX - left - ballRadius, 0),
-    width - ball.offsetWidth
-  );
-  const y = Math.min(
-    Math.max(clientY - top - ballRadius, 0),
-    height - ball.offsetHeight
-  );
-  Object.assign(ball.style, { left: `${x}px`, top: `${y}px` });
-});
 
 // DINO
+
+
 const dino = document.getElementById("dino");
 const cactus = document.getElementById("cactus");
 const startBtn = document.getElementById("startBtn");
@@ -247,6 +213,79 @@ stopBtn.onclick = stopGame;
 window.onload = () => {
   cactus.style.animationPlayState = "paused";
 };
+
+
+
+// Футбол
+
+
+const field = document.getElementById("field");
+const ball = document.getElementById("ball");
+const ballRadius = ball.offsetWidth / 2;
+field.addEventListener("click", ({ clientX, clientY }) => {
+  const { left, top, width, height } = field.getBoundingClientRect();
+  const x = Math.min(
+    Math.max(clientX - left - ballRadius, 0),
+    width - ball.offsetWidth
+  );
+  const y = Math.min(
+    Math.max(clientY - top - ballRadius, 0),
+    height - ball.offsetHeight
+  );
+  Object.assign(ball.style, { left: `${x}px`, top: `${y}px` });
+});
+
+
+
+// Введіть 3 числа
+
+
+const input1 = document.getElementById("input1");
+const input2 = document.getElementById("input2");
+const input3 = document.getElementById("input3");
+const output = document.getElementById("myParagraph");
+
+// Додаємо слухачі подій input
+input1.addEventListener("input", handleInput);
+input2.addEventListener("input", handleInput);
+input3.addEventListener("input", handleInput);
+
+function handleInput() {
+  const val1 = input1.value.trim();
+  const val2 = input2.value.trim();
+  const val3 = input3.value.trim();
+
+  const num1 = parseFloat(val1);
+  const num2 = parseFloat(val2);
+  const num3 = parseFloat(val3);
+
+  if (val1 === "" || val2 === "" || val3 === "") {
+    output.textContent = "Введіть усі три числа.";
+    return;
+  }
+
+  if (isNaN(num1) || isNaN(num2) || isNaN(num3)) {
+    output.textContent = "Будь ласка, введіть тільки числові значення.";
+    return;
+  }
+
+  const max = Math.max(num1, num2, num3);
+  output.textContent = `Найбільше число, яке ви ввели - ${max}`;
+}
+
+
+
+// Наша команда
+
+
+// new Swiper('.buttton_click_slide_box', {
+//   navigation: {
+//     nextEl: '.swiper-button-next',
+//     prevEl: '.swiper-button-prev'
+//   },
+// });
+
+
 
 //Обери вченого/их
 
@@ -338,141 +377,153 @@ const scientists = [
 ];
 
 let ageSum = scientists.reduce((sum, scientists) => {
-  return sum + (scientists.born - scientists.dead);
-});
+    return sum + (scientists.born - scientists.dead);
+  });
+  
+  //1
+  const scientistItemList = document.querySelectorAll(".item-list");
+  const scientistswasborn = document.getElementById("list1");
+  scientistswasborn.addEventListener("click", (event) => {
+    let scientistsNot19th = scientists.filter(
+      (scientist) => scientist.born >= 1801
+    );
+    scientistItemList.forEach((element, idx) => {
+      if (scientistsNot19th[idx]) {
+        element.textContent = `${scientistsNot19th[idx].name} ${scientistsNot19th[idx].surname}`;
+      }
+    });
+  });
 
-const scientistItemList = document.querySelectorAll(".item-list");
-
-// 1
-
-const scientistswasborn = document.getElementById("list1");
-scientistswasborn.addEventListener("click", (event) => {
-  let scientistsNot19th = scientists.filter(
-    (scientist) => scientist.born >= 1801
-  );
-  scientistItemList.forEach((element, idx) => {
-    if (scientistsNot19th[idx]) {
-      element.textContent = `${scientistsNot19th[idx].name} ${scientistsNot19th[idx].surname}`;
+  //2
+  const sorfOfNames = document.getElementById("list2");
+  sorfOfNames.addEventListener("click", (event) => {
+    const filteredScientists = scientists.sort((a, b) =>
+      a.name.localeCompare(b.name)
+    );
+    let albertEinsteinWasBorn = scientists.find(
+      (scientist) => scientist.surname === "Einstein"
+    );
+  
+    if (albertEinsteinWasBorn && scientistItemList.length > 0) {
+      scientistItemList[0].style.backgroundImage = `url("${albertEinsteinWasBorn.url}")`;
     }
   });
-});
+  //3
+  const sortOfDaysGone = document.getElementById("list3");
+  sortOfDaysGone.addEventListener("click", (event) => {
+    let sortedByAge = scientists.sort(
+      (a, b) => a.dead - a.born - (b.dead - b.born)
+    );
+    scientistItemList.forEach((element, idx) => {
+      if (sortedByAge[idx]) {
+        element.textContent = `${sortedByAge[idx].name} ${sortedByAge[idx].surname}`;
+      }
+    });
+  });
 
-//2
-
-const sorfOfNames = document.getElementById("list2");
-sorfOfNames.addEventListener("click", (event) => {
-  const filteredScientists = scientists.sort((a, b) =>
-    a.name.localeCompare(b.name)
-  );
-
-  let albertEinsteinWasBorn = scientists.find(
-    (scientist) => scientist.surname === "Einstein"
-  );
-
-  if (albertEinsteinWasBorn && scientistItemList.length > 0) {
-    scientistItemList[0].style.backgroundImage = `url("${albertEinsteinWasBorn.url}")`;
-  }
-});
-
-//3
-
-const sortOfDaysGone = document.getElementById("list3");
-sortOfDaysGone.addEventListener("click", (event) => {
-  let sortedByAge = scientists.sort(
-    (a, b) => a.dead - a.born - (b.dead - b.born)
-  );
-  scientistItemList.forEach((element, idx) => {
-    if (sortedByAge[idx]) {
-      element.textContent = `${sortedByAge[idx].name} ${sortedByAge[idx].surname}`;
+  //4
+  const wasBornLeiter = document.getElementById("list4");
+  wasBornLeiter.addEventListener("click", () => {
+    scientistItemList.forEach((element) => {
+      element.textContent = "";
+    });
+    let youngestScientist = scientists.reduce(
+      (youngest, scientist) =>
+        scientist.born > youngest.born ? scientist : youngest,
+      scientists[0]
+    );
+  
+    if (scientistItemList.length > 0) {
+      scientistItemList[0].textContent = `${youngestScientist.name} ${youngestScientist.surname}`;
     }
   });
-});
 
-//4
-
-const wasBornLeiter = document.getElementById("list4");
-wasBornLeiter.addEventListener("click", () => {
-  scientistItemList.forEach((element) => {
-    element.textContent = "";
+  //5
+  
+  const einsteinBorn = document.getElementById("list5");
+  
+  einsteinBorn.addEventListener("click", () => {
+    scientistItemList.forEach((element) => {
+      element.textContent = "";
+    });
+    let albertEinsteinWasBorn = scientists.find(
+      (scientist) => scientist.surname === "Einstein"
+    );
   });
-  let youngestScientist = scientists.reduce(
-    (youngest, scientist) =>
-      scientist.born > youngest.born ? scientist : youngest,
-    scientists[0]
-  );
-
-  if (scientistItemList.length > 0) {
-    scientistItemList[0].textContent = `${youngestScientist.name} ${youngestScientist.surname}`;
-  }
-});
-
-// 5
-
-const einsteinBorn = document.getElementById("list5");
-einsteinBorn.addEventListener("click", () => {
-  scientistItemList.forEach((element) => {
-    element.textContent = "";
-  });
-
-  let albertEinsteinWasBorn = scientists.find(
-    (scientist) => scientist.surname === "Einstein"
-  );
-});
-
-const secondNameC = document.getElementById("list6");
-secondNameC.addEventListener("click", (event) => {
-  scientistItemList.forEach((element) => {
-    element.textContent = "";
-  });
-  const scientistsWithC = scientists.filter(
-    (scientist) => scientist.surname[0] === "C"
-  );
-  scientistItemList.forEach((element, idx) => {
-    if (scientistsWithC[idx]) {
-      element.textContent = `${scientistsWithC[idx].name} ${scientistsWithC[idx].surname}`;
-    }
-  });
-});
-
-const filterOfNamesA = document.getElementById("list7");
-filterOfNamesA.addEventListener("click", (event) => {
-  const updatedScientists = scientists.filter(
-    (scientist) => scientist.name[0] !== "A"
-  );
-  scientistItemList.forEach((element) => {
-    element.textContent = "";
-  });
-  scientistItemList.forEach((element, idx) => {
-    if (updatedScientists[idx]) {
-      element.textContent = `${updatedScientists[idx].name} ${updatedScientists[idx].surname}`;
-    }
-  });
-});
-
-//8
-
-const findTheScientistsHadLife = document.getElementById("list8");
-findTheScientistsHadLife.addEventListener("click", () => {});
-
-const firstLetter = document.getElementById("list9");
-firstLetter.addEventListener("click", (event) => {
-  console.log();
-});
-
-const namesFirstLetter = document.getElementById("list9");
-
-namesFirstLetter.addEventListener("click", () => {
-  const filteredScientists = scientists.filter((scientist) =>
-    scientist.name[0].toUpperCase()
-  );
-
-  scientistItemList.forEach((element) => {
-    element.textContent = "";
+  
+  //6
+  const secondNameC = document.getElementById("list6");
+  secondNameC.addEventListener("click", (event) => {
+    scientistItemList.forEach((element) => {
+      element.textContent = "";
+    });
+    const scientistsWithC = scientists.filter(
+      (scientist) => scientist.surname[0] === "C"
+    );
+    scientistItemList.forEach((element, idx) => {
+      if (scientistsWithC[idx]) {
+        element.textContent = `${scientistsWithC[idx].name} ${scientistsWithC[idx].surname}`;
+      }
+    });
   });
 
-  scientistItemList.forEach((element, idx) => {
-    if (filteredScientists[idx]) {
-      element.textContent = `${filteredScientists[idx].name} ${filteredScientists[idx].surname}`;
-    }
+  //7
+  const filterOfNamesA = document.getElementById("list7");
+  filterOfNamesA.addEventListener("click", (event) => {
+    const updatedScientists = scientists.filter(
+      (scientist) => scientist.name[0] !== "A"
+    );
+    scientistItemList.forEach((element) => {
+      element.textContent = "";
+    });
+    scientistItemList.forEach((element, idx) => {
+      if (updatedScientists[idx]) {
+        element.textContent = `${updatedScientists[idx].name} ${updatedScientists[idx].surname}`;
+      }
+    });
   });
-});
+
+  //8
+  const findTheScientistsHadLife = document.getElementById("list8");
+  findTheScientistsHadLife.addEventListener("click", (event) => {});
+  const firstLetter = document.getElementById("list9");
+  firstLetter.addEventListener("click", (event) => {
+    console.log();
+  });
+  const namesFirstLetter = document.getElementById("list9");
+  
+  namesFirstLetter.addEventListener("click", () => {
+    const filteredScientists = scientists.filter(
+      (scientist) =>
+        scientist.name[0].toUpperCase() === scientist.surname[0].toUpperCase()
+    );
+  
+    scientistItemList.forEach((element) => {
+      element.textContent = "";
+    });
+  
+    scientistItemList.forEach((element, idx) => {
+      if (filteredScientists[idx]) {
+        element.textContent = `${filteredScientists[idx].name} ${filteredScientists[idx].surname}`;
+      }
+    });
+  });
+
+
+//модалка
+let buttonForModal = document.querySelector(".buttons");
+  buttonForModal.addEventListener("click", (event) => {
+    event.preventDefault();
+    modal.style.display = "block";
+  });
+  let closeModal = document.getElementById("modal_close");
+  closeModal.addEventListener("click", (event) => {
+    event.preventDefault();
+    modal.style.display = "none";
+  });
+  let modalBackgroung = document.getElementById("modal_backgroung");
+  
+  // modalBackgroung.onclick = closeModal.onclick = closeModal;
+  modalBackgroung.addEventListener("click", (event) => {
+    modal.style.display = "none";
+  });
